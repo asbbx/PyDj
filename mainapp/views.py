76b7d19754cb17f68ff59e29c1176488a81e52f4
andrew <cdgred@rambler.ru>
2022-12-08 13:42:16 +0300

    Loops in templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -28,6 +28,7 @@ class NewsView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['news_title'] = 'Громкий новостной заголовок'
-        context['news_preview'] = 'Предварительное описание, которое интересует каждого' 
+        context['news_preview'] = 'Предварительное описание, которое интересует каждого'
+        context['range'] = range(5)
         return context
     
